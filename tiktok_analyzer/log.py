9df6168322b6f96ad99876152e611bf3300589ab
Zakir Dzhamaliddinov <pfrbhxbr@gmail.com>
2021-08-24 21:51:51 +0300

    Fixed log issues

--- tiktok_analyzer/log.py
+++ tiktok_analyzer/log.py
@@ -3,14 +3,14 @@ from datetime import datetime
 from pathlib import Path
 
 LOG_FOLDER_PATH = 'logs'
-LOG_FILENAME = datetime.now().strftime(
-    '{}%Y_%m_%d_%H:%M:%S.log'.format(LOG_FOLDER_PATH)
-)
+LOG_FILENAME = datetime.now().strftime('%Y_%m_%d_%H:%M:%S.log')
+LOG_FILEPATH = f'{LOG_FOLDER_PATH}/{LOG_FILENAME}'
+
 LOG_FILEMODE = 'w'
 LOG_FORMAT = '%(asctime)s:%(levelname)s - %(message)s'
 
 
-def configure_logging(log_filename=LOG_FILENAME,
+def configure_logging(log_filename=LOG_FILEPATH,
                       lof_filemode=LOG_FILEMODE,
                       log_format=LOG_FORMAT):
     Path(LOG_FOLDER_PATH).mkdir(parents=True, exist_ok=True)
